3de3604ebab28d13bd3918404a1726d333f9365b
x <109935338+xzrvr@users.noreply.github.com>
2023-01-19 13:23:54 +1300

    Update commit.py

--- commit.py
+++ commit.py
@@ -1,12 +1,3 @@
 import os
-
-ip = int(input("how many times u wanna commit? \n"))
-autoPush = input("autopush when committed? (y/n) \n")
-
-for i in range(ip):
+while True:
 	os.system(f'git commit --allow-empty -m "commit {i}"')	
-
-print("Commited " + str(ip) + " times")
-
-if autoPush == "y":
-	os.system('git push')
